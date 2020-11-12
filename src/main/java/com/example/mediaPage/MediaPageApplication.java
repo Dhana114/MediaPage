baa56b6a2259a4ea55efbb8606b268682bff3423
dhana <ponugubatidhanalakshmi4444@gmail.com>
2020-11-12 22:11:29 +0100

    initial commit

---
+++ src/main/java/com/example/mediaPage/MediaPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediaPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mediaPage/MediaPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediaPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
