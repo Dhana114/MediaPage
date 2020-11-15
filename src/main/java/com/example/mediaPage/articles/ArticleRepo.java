d2ce2bf43804df240c1af6260fefd505671b44ae
dhana <ponugubatidhanalakshmi4444@gmail.com>
2020-11-15 09:37:10 +0100

    First version of the project

---
+++ src/main/java/com/example/mediaPage/articles/ArticleRepo.java
@@ -0,0 +1,9 @@
+package com.example.mediaPage.articles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepo extends JpaRepository<Article, Long> {
+
+}
